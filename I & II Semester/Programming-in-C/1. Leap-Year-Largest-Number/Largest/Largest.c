b06e5c8837ac602abcbbc791242b8a6ebef4236c
Rewant Soni <rewant.soni@gmail.com>
2017-11-26 22:40:18 +0530

    Updated (#3)
    * Updated
    
    * List of Programs
    
    * deleted DS_Store

--- I & II Semester/Programming-in-C/1. Leap-Year-Largest-Number/Largest/Largest.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main(){
-  int a,b,c;
-  printf("Enter three numbers:\n ");
-  scanf("%d%d%d",&a,&b,&c);
-  ((a>b)&&(a>c))?printf("\n%d is largest",a):(b>c)?printf("\n%d is the largest",b):printf("\n%d is the largest",c);
-}
