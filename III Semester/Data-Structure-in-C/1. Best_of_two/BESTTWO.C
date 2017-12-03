5e5b6270b3977ea38764bbb6f5debaf21a01bc8b
Akshaj Jain <akshaj.jain@gmail.com>
2017-12-03 11:12:05 +0530

    Comments #1

--- III Semester/Data-Structure-in-C/1. Best_of_two/BESTTWO.C
+++ III Semester/Data-Structure-in-C/1. Best_of_two/BESTTWO.C
@@ -1,3 +1,4 @@
+/* Program to get the average of  two largest numbers */
 #include<stdio.h>
 #include<conio.h>
 void main()
@@ -5,12 +6,14 @@ void main()
 	int a[100],n,i,l1,l2=-999;
 	float avg;
 	clrscr();
-	printf("Enter the number of elements\n");
+	printf("Enter the number of elements\n"); 
 	scanf("%d",&n);
+	/* Retrieving array */
 	printf("\nEnter %d elements: ", n);
 	for(i=0; i<n; i++)
 	{ scanf("%d",&a[i]);}
 	l1=a[0];
+	/* Checking for largest and second largest numbers*/
 	for (i=1; i<n; i++)
 	{ if(a[i]>l1)
 	  l1=a[i];
@@ -20,6 +23,7 @@ void main()
 	{ if(a[i]>l2&&l1!=a[i])
 	  l2=a[i];
 	}
+	/* Calculating average and printing it */
 	printf("Largest Number is: %d\nSecond largest Number is: %d", l1,l2);
 	avg=(float)(l1+l2)/2;
 	printf("\nAverage of Best two Number is: %f", avg);
