eee8f30332989a51252a39637de32a0bec1b4ff8
Rishabh Lakhotia <rishabhlakhotia227@gmail.com>
2017-12-06 02:13:33 +0530

    Improved Indentation

--- III Semester/Data-Structure-in-C/10. Max_Heap/maxHeap.c
+++ III Semester/Data-Structure-in-C/10. Max_Heap/maxHeap.c
@@ -1,59 +1,56 @@
-#include<stdio.h>
-#include<conio.h>
-#include<process.h>
-int a[100],n,i,root,c,temp,ch;
-void createheap(int i)
-{
-if(i==0) //location is zero
-{
-root=i;
-return;
-}
-else
-{
-c=i;
-while(c!=0)
-{
-root=(c-1)/2;
-if(a[root]<=a[c]) //parent is less than child swap
-{
-temp=a[root];
-a[root]=a[c];
-a[c]=temp;
-}
-c=root;
-}
-}
+#include <stdio.h>
+#include <conio.h>
+#include <process.h>
+
+int a[100], n, i, root, c, temp, ch;
+
+void createheap(int i) {
+    if (i == 0) //location is zero
+    {
+        root = i;
+        return;
+    } else {
+        c = i;
+        while (c != 0) {
+            root = (c - 1) / 2;
+            if (a[root] <= a[c]) //parent is less than child swap
+            {
+                temp = a[root];
+                a[root] = a[c];
+                a[c] = temp;
+            }
+            c = root;
+        }
+    }
 }
 
-void display()
-{
-printf("Max heap\n ");
-for(i=0;i<n;i++)
-printf("%d\t",a[i]);
+void display() {
+    printf("Max heap\n ");
+    for (i = 0; i < n; i++)
+        printf("%d\t", a[i]);
 }
-void main()
-{
-clrscr();
-for(;;)
-{
-printf("1. create Max Heap\n2. display\n3. exit\n");
-printf("enter ur choice\n");
-scanf("%d",&ch);
-switch(ch)
-{
-case 1:printf("enter the number of nodes\n");
-scanf("%d",&n);
-printf("enter the elements\n");
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-createheap(i);
-}
-break;
-case 2:display();
-break;
-default:exit(0);
-}
-}
+
+void main() {
+    clrscr();
+    for (;;) {
+        printf("1. create Max Heap\n2. display\n3. exit\n");
+        printf("enter ur choice\n");
+        scanf("%d", & ch);
+        switch (ch) {
+        case 1:
+            printf("enter the number of nodes\n");
+            scanf("%d", & n);
+            printf("enter the elements\n");
+            for (i = 0; i < n; i++) {
+                scanf("%d", & a[i]);
+                createheap(i);
+            }
+            break;
+        case 2:
+            display();
+            break;
+        default:
+            exit(0);
+        }
+    }
 }
--- III Semester/Data-Structure-in-C/11. BST_Traversals/BST.C
+++ III Semester/Data-Structure-in-C/11. BST_Traversals/BST.C
@@ -1,58 +1,55 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 #define SIZE 100
-int a[SIZE],c;
-void bst(int ele)
-{
-if(a[0]==NULL)
-{
-a[0]=ele;
-return;
+
+int a[SIZE], c;
+void bst(int ele) {
+    if (a[0] == NULL) {
+        a[0] = ele;
+        return;
+    }
+    c = 0;
+    while (a[c] != NULL) {
+        if (ele < a[c])
+            c = 2 * c + 1;
+        else
+            c = 2 * c + 2;
+    }
+    a[c] = ele;
 }
-c=0;
-while(a[c]!=NULL)
-{
-if(ele<a[c])
-c=2*c+1;
-else
-c=2*c+2;
+
+void display() {
+    int i;
+    for (i = 0; i < SIZE; i++) {
+        if (a[i] == NULL)
+            continue;
+        printf("a[%d]=%d\n", i, a[i]);
+    }
 }
-a[c]=ele;
-}
-void display()
-{
-int i;
-for(i=0;i<SIZE;i++)
-{
-if(a[i]==NULL)
-continue;
-printf("a[%d]=%d\n",i,a[i]);
-}
-}
-void main()
-{
-int ch,i,j,ele;
-printf("\n1.bst\n2.display\n");
-for(;;)
-{
-printf("\nEnter choice\n");
-scanf("%d",&ch);
-switch(ch)
-{
-case 1: for(i=0;i<SIZE;i++) a[i]=NULL;
-printf("\enter no of ele to enter\n");
-scanf("%d",&j);
-printf("\nEnter the array of elements\n");
-for(i=0;i<j;i++)
-{
-scanf("%d",&ele);
-bst(ele);
-}
-break;
-case 2:display();
-break;
-case 3: exit(0);
-}
-}
+
+void main() {
+    int ch, i, j, ele;
+    printf("\n1.bst\n2.display\n");
+    for (;;) {
+        printf("\nEnter choice\n");
+        scanf("%d", & ch);
+        switch (ch) {
+        case 1:
+            for (i = 0; i < SIZE; i++) a[i] = NULL;
+            printf("\enter no of ele to enter\n");
+            scanf("%d", & j);
+            printf("\nEnter the array of elements\n");
+            for (i = 0; i < j; i++) {
+                scanf("%d", & ele);
+                bst(ele);
+            }
+            break;
+        case 2:
+            display();
+            break;
+        case 3:
+            exit(0);
+        }
+    }
 }
