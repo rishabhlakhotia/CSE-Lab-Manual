131c0e9bbac202dc4be39a3089eabcbfe2f15f17
Rishabh Lakhotia <rishabhlakhotia227@gmail.com>
2017-12-06 01:33:38 +0530

    Improved Indentation

--- III Semester/Data-Structure-in-C/1. Best_of_two/BESTTWO.C
+++ III Semester/Data-Structure-in-C/1. Best_of_two/BESTTWO.C
@@ -1,28 +1,34 @@
-// Program to get the average of  two largest numbers 
+// Program to get the average of  two largest numbers
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
-	int a[100],n,i,l1,l2=-999;
+	int a[100], n, i, l1, l2 = -999;
 	float avg;
+
 	clrscr();
-	printf("Enter the number of elements\n"); 
+	printf("Enter the number of elements\n");
 	scanf("%d",&n);
+
 	// Retrieving array
 	printf("\nEnter %d elements: ", n);
-	for(i=0; i<n; i++)
-	{ scanf("%d",&a[i]);}
-	l1=a[0];
-	// Checking for largest and second largest numbers 
-	for (i=1; i<n; i++)
-	{ if(a[i]>l1)
-	  l1=a[i];
+	for(i=0; i<n; i++) {
+		scanf("%d",&a[i]);
 	}
+	l1=a[0];
+
+	// Checking for largest and second largest numbers
+	for (i=1; i<n; i++) {
+		if(a[i]>l1)
+	  	l1=a[i];
+	}
+
 	l2=a[0];
-	for(i=1; i<n; i++)
-	{ if(a[i]>l2&&l1!=a[i])
+	for(i=1; i<n; i++) {
+		if(a[i]>l2&&l1!=a[i])
 	  l2=a[i];
 	}
+
 	// Calculating average and printing it
 	printf("Largest Number is: %d\nSecond largest Number is: %d", l1,l2);
 	avg=(float)(l1+l2)/2;
