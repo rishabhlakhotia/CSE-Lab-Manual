98d68c287cd30f5cf5fb5fef1d52857b190ffdbb
Anush DP <43229257+Anush-DP@users.noreply.github.com>
2019-03-13 20:12:21 +0530

    Update BSearch.asm

--- IV Semester/Microprocessor_Lab/1A_Binary_Search/BSearch.asm
+++ IV Semester/Microprocessor_Lab/1A_Binary_Search/BSearch.asm
@@ -9,25 +9,29 @@
 .code
 mov ax,@data
 mov ds,ax
+
 lea dx,m
 call printf
-call read8
+
+call read8 ;read 8 bit number
+
 mov key,al
-mov bl,l;bubble sort
+mov bl,l   ;bubble sort
 dec bl
 xor bh,bh
 l2:
-mov cx,bx
-lea si,A
-l1: mov al,[si]
-inc si
-cmp al,[si]
-jbe noswap
-xchg al,[si]
-mov [si-1],al
-noswap:loop l1
-dec bx
+    mov cx,bx
+    lea si,A
+    l1: mov al,[si]
+        inc si
+        cmp al,[si]
+        jbe noswap
+        xchg al,[si]
+        mov [si-1],al
+        noswap:loop l1
+        dec bx
 jnz l2;sorting ends
+
 ;binary search
 lea si,A
 mov di,si
@@ -36,34 +40,36 @@ xor bh,bh
 add di,bx
 dec di
 lp1:cmp si,di
-ja nf
-mov bx,si
-add bx,di
-shr bx,01h
-mov al,key
-cmp al,[bx]
-jz found
-ja nlow
-mov di,bx
-dec di
-jmp lp1
-nlow:mov si,bx
-inc si
+    ja nf
+    mov bx,si
+    add bx,di
+    shr bx,01h
+    mov al,key
+    cmp al,[bx]
+    jz found
+    ja nlow
+    mov di,bx
+    dec di
+    jmp lp1
+    nlow:mov si,bx
+    inc si
 jmp lp1
 found:lea dx,m1
-call printf
-jmp exit
+    call printf
+    jmp exit
 nf:lea dx,m2
-call printf
+    call printf
 exit:
-mov ah,4ch
-int 21h
-printf proc near
+    mov ah,4ch
+    int 21h
+    
+printf proc near  ; procedure for printf
 mov ah,09h
 int 21h
 ret 
 printf endp
-read8 proc near
+
+read8 proc near  ;procedure for reading 8 bit number
 mov ah,01h
 int 21h
 mov bh,al
@@ -75,4 +81,5 @@ mov ah,bh
 aad
 ret
 read8 endp
+
 end
